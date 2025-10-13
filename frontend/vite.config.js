ab8d4d436d9bf3929cf31ddf8cd9f9f6a34a3142
samyou02 <samyuktha@speshway.com>
2025-10-13 16:53:33 +0530

    samyu....

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
-    port: 5180,
+    port: 5173,
     strictPort: true
   }
 })
